40af574d4f32dd312689b4ec398bf6ef116cd1f0
Tomas Ramirez <tiramirez@uc.cl>
2025-09-20 17:21:02 -0300

    [add] change sleep time to 5

--- lambdas/lambda_test/handler.py
+++ lambdas/lambda_test/handler.py
@@ -5,7 +5,7 @@ logger = logger.get_logger()
 
 def lambda_handler(event, context):
     logger.info("Program starting...")
-    time.sleep(10)  # Pause execution for 4 seconds
+    time.sleep(5)  # Pause execution for 4 seconds
     logger.info("Program continuing after 4-second delay.")
     
     return {"statusCode": 200, "body": "Hello"}
