32654730a5ab7f524dbdf2433bfa1a0b79d631a1
Tomas Ramirez <tiramirez@uc.cl>
2025-10-24 17:18:47 -0300

    [add] infrastructure as code

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "bucket_name" {
+  description = "Base name of the S3 bucket to store artifacts."
+  type        = string
+}
+
+variable "project_name" {
+  description = "Projects name use for tags"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "lambda_handler" {
+  description = "Lambda function handler"
+  type        = string
+  default     = "handler.lambda_handler"
+}
+
+variable "lambda_runtime" {
+  description = "Lambda runtime"
+  type        = string
+  default     = "python3.11"
+}
